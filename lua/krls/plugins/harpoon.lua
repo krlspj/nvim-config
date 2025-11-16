378311918d3fc0958352b8b1ca197ef87b01ef0f
krlspj <krls.pjds@gmail.com>
2025-11-16 14:20:36 +0100

    update harpoon 2

--- lua/krls/plugins/harpoon.lua
+++ lua/krls/plugins/harpoon.lua
@@ -1,23 +1,26 @@
 return {
   "ThePrimeagen/harpoon",
-  branch = "harpoon2",               -- v2 API
+  branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
-  event = "VeryLazy",
+
   config = function()
     local harpoon = require("harpoon")
-    harpoon:setup({})                -- required
+    harpoon:setup()                       -- REQUIRED
+
+    -- add / toggle quick-menu
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    -- jump to marks 1-4
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+
+    -- cycle through list
+    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
   end,
-  keys = {
-    -- mark / un-mark current file
-    { "<leader>a", function() require("harpoon"):list():add() end,
-      desc = "Harpoon add file" },
-    -- toggle quick-menu
-    { "<leader>o", function() require("harpoon"):list():toggle_quick_menu() end,
-      desc = "Harpoon quick menu" },
-    -- jump to marks 1-4  (change keys to taste)
-    { "<C-h>", function() require("harpoon"):list():select(1) end, desc = "Harpoon 1" },
-    { "<C-j>", function() require("harpoon"):list():select(2) end, desc = "Harpoon 2" },
-    { "<C-k>", function() require("harpoon"):list():select(3) end, desc = "Harpoon 3" },
-    { "<C-l>", function() require("harpoon"):list():select(4) end, desc = "Harpoon 4" },
-  },
 }
