695f7a7a34f356299d31bc78f7546f5742fdb633
cpujades <cpujades@student.42barcelona.com>
2025-12-06 17:52:11 +0100

    config for 42 school

--- init.lua
+++ init.lua
@@ -674,7 +674,7 @@ require('lazy').setup({
       --
       local servers = {
         clangd = {},
-        gopls = {},
+        --gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
